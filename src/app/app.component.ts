1d0de46435b24349a18d86534b3ebfbc5de8be7d
rsuryadi <rsuryadi@BRW08ICT52523L>
2018-03-29 13:28:12 +0300

    add sidemenu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,59 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+
+import { Platform, MenuController, Nav } from 'ionic-angular';
+
+import { HomePage } from '../pages/home/home';
+import { AdultsPage } from '../pages/adults/adults';
+import { KidsPage } from '../pages/kids/kids';
+import { DentistsPage } from '../pages/dentists/dentists';
+
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { TabsPage } from '../pages/tabs/tabs';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = TabsPage;
+  @ViewChild(Nav) nav: Nav;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    platform.ready().then(() => {
+  // make HelloIonicPage the root (or first) page
+  rootPage = HelloIonicPage;
+  pages: Array<{title: string, component: any}>;
+
+  constructor(
+    public platform: Platform,
+    public menu: MenuController,
+    public statusBar: StatusBar,
+    public splashScreen: SplashScreen
+  ) {
+    this.initializeApp();
+
+    // set our app's pages
+    this.pages = [
+      { title: 'Home', component: HomePage },
+      { title: 'Adults', component: AdultsPage },
+	  { title: 'Kids', component: KidsPage },
+	  { title: 'Dentists', component: DentistsPage },
+	  { title: 'Follow Us', component: DentistsPage },
+	  { title: 'Dentists', component: DentistsPage },
+	  { title: 'MapFrame', component: DentistsPage }
+    ];
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
     });
   }
-}
 
+  openPage(page) {
+    // close the menu when clicking a link from the menu
+    this.menu.close();
+    // navigate to the new page if it is not the current page
+    this.nav.setRoot(page.component);
+  }
+}
