3635094d7b9926b821dfcbce020a0fb647ae60e7
rsuryadi <rsuryadi@BRW08ICT52523L>
2018-03-29 14:15:38 +0300

    support for menu n tab both

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,14 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-
+export interface PageInterface {
+  title: string;
+  pageName: string;
+  tabComponent: any;
+  index: number;
+  icon: string;
+}
+ 
 @Component({
   templateUrl: 'app.html'
 })
@@ -21,8 +28,14 @@ export class MyApp {
 
   // make HomePage the root (or first) page
   rootPage = TabsPage;
-  pages: Array<{title: string, component: any}>;
-
+  //pages: Array<{title: string, component: any}>;
+  
+  pages: PageInterface[] = [
+    { title: 'Tab 1', pageName: 'TabsPage', tabComponent: 'Tab1Page', index: 0, icon: 'home' },
+    { title: 'Tab 2', pageName: 'TabsPage', tabComponent: 'Tab2Page', index: 1, icon: 'contacts' },
+    { title: 'Special', pageName: 'SpecialPage', icon: 'shuffle' },
+  ];
+  
   constructor(
     public platform: Platform,
     public menu: MenuController,
@@ -52,10 +65,47 @@ export class MyApp {
     });
   }
 
-  openPage(page) {
-    // close the menu when clicking a link from the menu
-    this.menu.close();
-    // navigate to the new page if it is not the current page
-    this.nav.setRoot(page.component);
+  // openPage(page) {
+    // // close the menu when clicking a link from the menu
+    // this.menu.close();
+    // // navigate to the new page if it is not the current page
+    // this.nav.setRoot(page.component);
+  // }
+  
+  openPage(page: PageInterface) {
+    let params = {};
+ 
+    // The index is equal to the order of our tabs inside tabs.ts
+    if (page.index) {
+      params = { tabIndex: page.index };
+    }
+ 
+    // The active child nav is our Tabs Navigation
+    if (this.nav.getActiveChildNav() && page.index != undefined) {
+      this.nav.getActiveChildNav().select(page.index);
+    } else {
+      // Tabs are not active, so reset the root page 
+      // In this case: moving to or from SpecialPage
+      this.nav.setRoot(page.pageName, params);
+    }
   }
+ 
+  isActive(page: PageInterface) {
+    // Again the Tabs Navigation
+    let childNav = this.nav.getActiveChildNav();
+ 
+    if (childNav) {
+      if (childNav.getSelected() && childNav.getSelected().root === page.tabComponent) {
+        return 'primary';
+      }
+      return;
+    }
+ 
+    // Fallback needed when there is no active childnav (tabs not active)
+    if (this.nav.getActive() && this.nav.getActive().name === page.pageName) {
+      return 'primary';
+    }
+    return;
+  }
+  
 }
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -15,7 +15,8 @@ export class TabsPage {
   tab3Root = KidsPage;
   tab4Root = DentistsPage;
 
-  constructor() {
-
+  constructor(navParams: NavParams) {
+    // Set the active tab based on the passed index from menu.ts
+    this.myIndex = navParams.data.tabIndex || 0;
   }
 }
